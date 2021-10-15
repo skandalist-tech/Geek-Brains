72e801f9af83c6205182b959bade75e042ecec3c
skandalist-tech <83368821+skandalist-tech@users.noreply.github.com>
2021-10-15 10:45:29 +0500

    Delete 1.1.py

--- WorkPython/1/1.1.py
+++
@@ -1,6 +0,0 @@
-duration = int(input("Введите время в секундах : "))
-second = duration % 60
-minutes = (duration // 60) % 60
-hours = (duration // 3600) % 24
-days = duration // 86400
-print("дней:", days, "часов:", hours, "минут:", minutes, "секунд:", second)
